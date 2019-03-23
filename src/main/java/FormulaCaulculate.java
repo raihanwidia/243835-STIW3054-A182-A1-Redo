4db89784f75c3e8827c6b9157e029d7e86fc39bd
Raihan Widia 243835 <raihankirana@gmail.com>
2019-03-23 20:33:01 +0800

    fifth commit : just found bug

--- src/main/java/FormulaCaulculate.java
+++ src/main/java/FormulaCaulculate.java
@@ -2,10 +2,10 @@
 public class FormulaCaulculate  {
 
 	
-	static CalculateUseCase a;
-	static CalculateTechnicalFactors b;
-	static CalculateExperienceFactors c;
-	static CalculateActors d;
+	CalculateUseCase a;
+	CalculateTechnicalFactors b;
+	CalculateExperienceFactors c;
+	CalculateActors d;
 	
 	int weightedActors;
 	int weightedUseCases;
@@ -27,30 +27,17 @@ public class FormulaCaulculate  {
 		ExperienceF = c.totalEF(); 
 	    formulatype = c.manhFormula();
 	}
-
-	public int Uucp () {
-		return weightedUseCases + weightedActors; 
-	}
-	
-	public double Tcf() {
-		return (0.01 * TechnicalF)+0.06;
-	}
-	
-	public double SzUC() {
-		int a = Uucp();
-		double b = Tcf();
-		return a * b;
-	}
-
-	public double ExpFa() {
-		return (-0.03 * TechnicalF)+1.4;
-	}
 	
 	
 	public double ucp () {
-		double a = SzUC();
-		double b = ExpFa();
-		return a*b;
+		int uucp = weightedUseCases + weightedActors ;
+		double TCF = (0.01 * TechnicalF)+0.6;
+		double SzUC = uucp * TCF;
+		double ExpFa = (-0.03 * ExperienceF)+1.4;
+		double ucp = SzUC * ExpFa;
+
+		
+		return ucp;
 	}
 	
     public double Manhours(){
